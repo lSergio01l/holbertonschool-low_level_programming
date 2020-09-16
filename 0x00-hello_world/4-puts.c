10d059240e1566006f2b2b39af74af6548659db7
Sergio <sergioasb628@gmail.com>
2020-09-16 22:25:44 +0000

    Puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-/*main - Print string*/
-/*return 0*/
+/*
+  *main - Print string
+  *return 0
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
