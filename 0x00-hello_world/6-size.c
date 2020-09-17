a99efe8e26c61683d642730b36ac460da83b417c
Sergio <sergioasb628@gmail.com>
2020-09-17 03:40:44 +0000

    Sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("Size of a char: %ld byte\n", sizeof(char))
+	printf("Size of an int: %ld byte\n", sizeof(int))
+	printf("Size of a long int: %ld byte\n", sizeof(long int))
+	printf("Size of a long long: %ld byte\n", sizeof(long long int))
+	printf("Size of a float: %ld byte\n", sizeof(float))
+}
