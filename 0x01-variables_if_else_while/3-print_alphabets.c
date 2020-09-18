0dea143f56eb9c745ccb12aa8eea31611156d1e8
Sergio <sergioasb628@gmail.com>
2020-09-18 20:25:41 +0000

    lower and uppercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,9 +8,9 @@ int main(void)
 	char a;
 	char z;
 
-	for (a = 'a'; a < 'z'; a++)
+	for (a = 'a'; a <= 'z'; a++)
 		putchar(a);
-	for (z = 'A'; z < 'Z'; z++)
+	for (z = 'A'; z <= 'Z'; z++)
 		putchar(z);
 	putchar('\n');
 	return (0);
