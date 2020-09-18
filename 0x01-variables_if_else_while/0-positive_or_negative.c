f3ea6fd00187e935228445d7a24c409aaa9996f6
Sergio <sergioasb628@gmail.com>
2020-09-18 01:52:42 +0000

    Positive, zero or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+/**
+* main - will assign a random number to the variable n each time it is executed
+* Return: 0
+*/
 int main(void)
 {
 	int n;
